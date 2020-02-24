fd2c1c09731c751d27d28d5956e82573452a8e21
CharlesWhj <1253297146@qq.com>
2020-02-24 19:38:29 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+// 从ui库中引入按钮
+import {Button} from 'antd-mobile'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="warning">按钮</Button>
     </div>
   );
 }
